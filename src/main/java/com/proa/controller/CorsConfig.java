aa95bcf1bcdbcbf88f8955b7291b3a508632a286
MarcoABN <102033851+MarcoABN@users.noreply.github.com>
2025-02-23 08:21:11 -0300

    23/02/2025

---
+++ src/main/java/com/proa/controller/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.proa.controller;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://52.21.85.137", "http://172.26.12.187") // Adicionando os dois IPs
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
